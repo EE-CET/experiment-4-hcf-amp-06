c2790cc0d59a3800fce4b27d357ce603ad2713a2
amrita-m-pillai <amritampillai06@gmail.com>
2026-01-18 20:35:29 +0530

    first commit

--- src/HCF.java
+++ src/HCF.java
@@ -1,5 +1,25 @@
-public class HCF {
-    
-        // TODO: Read two integers and print their HCF
-    
-}
+import java.util.Scanner;
+
+	public class HCF
+		{
+			public static void main(String [] args)
+			{
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter two numbers");
+				int a=sc.nextInt();
+				int b=sc.nextInt();
+				int small, hcf=1;
+				if(a>b)
+					small=b;
+				else
+					small=a;
+				for(int i=1;i<=small;i++)
+				{
+					if(a%i==0 && b%i==0)
+					{
+						hcf=i;
+					}
+				}
+				System.out.println(hcf);
+			}
+		}
